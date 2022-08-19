29e21b0c9dfbdcbd07a99ae9a70d628e9f165b16
frederikvdz <61236724+frederikvdz@users.noreply.github.com>
2022-08-19 15:04:14 +0200

    11ty-up-and-running
    Changes site from static pages to 11ty generated

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+
+const { DateTime } = require('luxon');
+
+
+module.exports = function(eleventyConfig){
+//take source files and serve them
+eleventyConfig.addPassthroughCopy('./src/style.css');
+eleventyConfig.addPassthroughCopy('./src/assets');
+eleventyConfig.addFilter("postDate", (dateObj) =>{
+  return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+});
+
+// Return your Object options:
+return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  }
+}
